2d9dc8478b28b08b6b946bde18d0064591e21e39
Yegor <yskuridov@gmail.com>
2021-11-07 19:55:11 -0500

    afasf

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'resultats';
+  uneclass = [
+    { nom: 'Brionna Ernser', note: 54 },
+    { nom: 'Rowan Terry', note: 67 },
+    { nom: 'Patricia Lehner', note: 45 },
+    { nom: 'Jade Kohler ', note: 98 },
+    { nom: 'Hattie Gleichner', note: 77 },
+    { nom: 'Alycia Gusikowski', note: 67 },
+    { nom: 'Travis Emmerich', note: 57 },
+    { nom: 'Jena Wiegand', note: 60 },
+    { nom: 'Vella Leffler', note: 66 },
+    { nom: 'Marcelle Morissette', note: 65 },
+    { nom: 'Allie Haley', note: 88 }
+  ];
+}
+
+
